e417a8dfd53bec24eea68971ade1766670cca691
minerkid08 <85370763+minerkid08@users.noreply.github.com>
2024-01-04 14:02:53 -0500

    g

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 function mouseClick(event){
     try{
         let a = new Renderer();
+        a.drawText("e", 0, 0);
+        a.drawText("e", 1, 0);
+        a.drawText("eee", 0, 1);
     }catch(e){
         alert(e + " : " + e.stack);
     }
@@ -19,6 +22,8 @@ class Renderer{
         this.canvas = document.getElementById("board");
         this.ctx = this.canvas.getContext("2d");
         this.ctx.font = "30px Courier New";
-        this.ctx.fillText("123456789123456789123456789123456789", 10, 50);
+    }
+    drawText(char, x, y, color){
+        this.ctx.fillText(char, x * 30, y * 30);
     }
 }
